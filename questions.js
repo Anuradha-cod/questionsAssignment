aadafda144db5c6c9dd41cdfddfde7440f6534fe
Anuradha <anuradhabansal728989@gmail.com>
2020-11-08 19:54:59 +0530

    5ques

---
+++ questions.js
@@ -0,0 +1,91 @@
+// 1. 
+// function findInt() {
+    
+// var arr = [3, 2, 5, 6, 7];
+// for(int i = 0; i < arr.length i++){
+//   int multiplication =1;
+//   for(let j = 0; j < arr.length j++){
+// int value = arr[i];
+// if (value != arr[j]){
+//     multiplication = multiplication*arr[j];
+//    }
+//   }  
+//   product.add(multiplication);
+// }
+// console.log(product.add);
+// }
+// findInt();
+
+
+
+
+// 3.
+
+
+// function decimalToBinary(num){
+//     var x = [];
+//     while(num>0){
+//         let rem = num%2
+//         if (rem == 0){
+//             x.push(1);
+//         } else if(rem ==1){
+//             x.push(0);
+//         }
+//         // x.push(rem);
+//         // console.log(rem)
+//          num = parseInt (num/2)
+//     }
+//     x.reverse();
+//     // console.log(x);
+//     return x;
+// }
+// // console.log(decimalToBinary(50))
+
+// function binaryToDecimal(num) {
+//     let result = 0;
+//     let length = num.length;
+//     for(let i=length-1; i>=0; i--){
+//         result = result + num[i] * Math.pow(2, length-1-i);
+//     }
+//     return result;
+//     // console.log(result);
+// }
+
+// function complementOfDecimal(num){
+//     const binaryOutput = decimalToBinary(num);
+//     const result = binaryToDecimal(binaryOutput);
+//     return result;
+// }
+
+// let result = complementOfDecimal(50);
+// console.log(result);
+
+
+
+// // 4.
+
+// let arr =[learnatdevskool];
+
+// const res = array => {
+//     for(let i =0; i >= array.length; i--){
+//         for(let j = 0; j < array.length; j++){
+//             if(i !== j && arr[i]==arr[j]){
+//             }
+//         }
+//         return -1
+//     }
+// };
+// console.log(arr);
+
+// 5. 
+
+// var sortColors = function(nums) {
+//     for(i = 0; i <Array.length; i++){
+//         while(i<=nums);
+//         shortColors.cur = nums[j];
+//         if (cur == 0)
+//         return
+//     }
+// };
+// console.log(shortColors);
+
